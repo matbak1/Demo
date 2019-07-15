9700d4b51af369bb4e0051669a840802713be23a
mateusz <mateusz.bakon@gmail.com>
2019-07-15 23:35:37 +0200

    add check to createAddress

--- src/main/java/com/example/demo/repository/AddressRepository.java
+++ src/main/java/com/example/demo/repository/AddressRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Address;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.expression.spel.ast.OpOr;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
